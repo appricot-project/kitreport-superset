db618c07aa182c18dd62a0a44dd5fdee1a9b4b09
Rorrian <nkunegina@yandex.ru>
2025-09-02 16:08:27 +0200

    feature/KITREPORT-3: added brand 'tri-kita'

---
+++ superset-frontend/branding/tri-kita/tri-kita.ts
@@ -0,0 +1,7 @@
+export const branding = {
+  appName: 'Три кита',
+  logoPath: '/static/assets/images/branding/tri-kita/tri-kita.png',
+
+  primaryColor: '#052E40',
+  secondaryColor: '#6F818C',
+};
